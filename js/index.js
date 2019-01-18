f9bcd36f61e69a0e9ca7f263cdb30d47fbd6001d
LiuZe <2411509324@qq.com>
2019-01-18 12:33:37 +0800

    updata style

--- js/index.js
+++ js/index.js
@@ -1,36 +1,7 @@
-const vm = new Vue({
+new Vue({
     el: '#app',
     data: {
-        title: 'Hello Vue.js',
-        users: [{
-                name: 'liuze',
-                age: 22,
-                six: '男'
-            },
-            {
-                name: '张三',
-                age: '24',
-                six: '男'
-            },
-            {
-                name: '李四',
-                age: '32',
-                six: '男'
-            },
-            {
-                name: '小红',
-                age: '18',
-                six: '女'
-            }
-        ],
-        isAddUser: false,
-        user: {
-            name: '',
-            age: '',
-            six: ''
-        },
-        show: true,
-        text: ''
+        title: 'Hello Vue.js'
     },
     mounted() {
         window.document.title = this.title;
@@ -39,32 +10,9 @@ const vm = new Vue({
 
     },
     methods: {
-        addUser() {
-            Object.keys(this.user).forEach(key => {
-                if (this.user[key]) this.isAddUser = true;
-                else this.isAddUser = false;
-            });
-            if (this.isAddUser) {
-                this.users.push(this.user);
-            }
-        },
-        removeUser(index) {
-            // console.log(index);
-            this.users.splice(index, 1);
-        },
-        toogleBox() {
-            this.show = !this.show;
-        }
+
     },
     watch: {
-        text(newValue, oldValue) {
-            console.log(newValue)
-        },
-        user: {
-            handler(newValue, oldValue) {
-                console.log(newValue);
-            },
-            deep: true
-        }
-    },
+
+    }
 })
